7480560b50a3a34a2251588570206511d95dc025
Manav kumar jha <manavjha77@gmail.com>
2026-07-29 18:47:12 +0530

    first portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+    strings: ['I am Manav.', 'Software Engineer.', 'Web Developer.'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+});
+const btn=document.querySelector("#build_project");
+btn.addEventListener("click",()=>{
+    window.location.href="build.html";
+})
